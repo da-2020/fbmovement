a6df927586a093d406c5dbb9e4a0c6c59d203bdf
Daniel Allen <daniel.allen@caci.com>
2020-12-23 21:00:50 -0500

    make it better3

--- intro.py
+++ intro.py
@@ -29,34 +29,6 @@ app.layout = html.Div([
 
 ])
 
-
-# ------------------------------------------------------------------------------
-# Connect the Plotly graphs with Dash Components
-@app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='my_bee_map', component_property='figure')],
-    [Input(component_id='slct_year', component_property='value')]
-)
-def update_graph():
-
-    dff = df.copy()
-
-    # Plotly Express
-    fig = px.choropleth(
-        data_frame=dff,
-        locationmode='USA-states',
-        locations='state_code',
-        scope="usa",
-        color='Pct of Colonies Impacted',
-        hover_data=['State', 'Pct of Colonies Impacted'],
-        color_continuous_scale=px.colors.sequential.YlOrRd,
-        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
-        template='plotly_dark'
-    )
-
-    return container, fig
-
-
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     app.run_server(debug=True)
