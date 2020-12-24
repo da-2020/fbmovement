a65cecb4d88cdb51a07aa37edce662f984bbec3f
Daniel Allen <daniel.allen@caci.com>
2020-12-23 20:22:02 -0500

    make it better

--- intro.py
+++ intro.py
@@ -8,7 +8,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
-
+server = app.server
 # ------------------------------------------------------------------------------
 # Import and clean data (importing csv into pandas)
 df = pd.read_csv("intro_bees.csv")
