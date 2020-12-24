5c53a0ed647ac67ae7713b2c6be2c8e643446f29
Daniel Allen <daniel.allen@caci.com>
2020-12-23 20:53:23 -0500

    make it better2

--- intro.py
+++ intro.py
@@ -11,9 +11,9 @@ app = dash.Dash(__name__)
 server = app.server
 # ------------------------------------------------------------------------------
 # Import and clean data (importing csv into pandas)
-df = pd.read_csv("intro_bees.csv")
+df = pd.read_csv("FBLOCDENSE_IR_STRIP_AG.csv")
 
-df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+df = df.groupby(['date', 'mov-dev', 'no-move-ratio'])[['no-move-ratio']].mean()
 df.reset_index(inplace=True)
 print(df[:5])
 
@@ -21,23 +21,11 @@ print(df[:5])
 # App layout
 app.layout = html.Div([
 
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+    html.H1("Template Application Daniel Allen", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="slct_year",
-                 options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018}],
-                 multi=False,
-                 value=2015,
-                 style={'width': "40%"}
-                 ),
-
-    html.Div(id='output_container', children=[]),
     html.Br(),
 
-    dcc.Graph(id='my_bee_map', figure={})
+    dcc.Graph(id='irq_ag', figure={})
 
 ])
 
@@ -49,15 +37,9 @@ app.layout = html.Div([
      Output(component_id='my_bee_map', component_property='figure')],
     [Input(component_id='slct_year', component_property='value')]
 )
-def update_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
-
-    container = "The year chosen by user was: {}".format(option_slctd)
+def update_graph():
 
     dff = df.copy()
-    dff = dff[dff["Year"] == option_slctd]
-    dff = dff[dff["Affected by"] == "Varroa_mites"]
 
     # Plotly Express
     fig = px.choropleth(
@@ -71,24 +53,6 @@ def update_graph(option_slctd):
         labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
         template='plotly_dark'
     )
-
-    # Plotly Graph Objects (GO)
-    # fig = go.Figure(
-    #     data=[go.Choropleth(
-    #         locationmode='USA-states',
-    #         locations=dff['state_code'],
-    #         z=dff["Pct of Colonies Impacted"].astype(float),
-    #         colorscale='Reds',
-    #     )]
-    # )
-    #
-    # fig.update_layout(
-    #     title_text="Bees Affected by Mites in the USA",
-    #     title_xanchor="center",
-    #     title_font=dict(size=24),
-    #     title_x=0.5,
-    #     geo=dict(scope='usa'),
-    # )
 
     return container, fig
 
