21e5677bf2439eebf2494779b9c2889d085390f2
da-2020 <76570747+da-2020@users.noreply.github.com>
2021-01-12 10:58:17 -0500

    initial launch7

--- intro.py
+++ intro.py
@@ -13,7 +13,19 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-app = dash.Dash(__name__)
+# external CSS stylesheets
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 # ------------------------------------------------------------------------------
 # Import and clean data (importing csv into pandas)
